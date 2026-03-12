cfca0510ac42172c171c3f28799c69a08ab4d717
Shilpa-Pal <shilpapal1721@gmail.com>
2026-03-12 06:28:37 +0530

    corrected the code .

--- two_pointer/three_sum/optimal.js
+++ two_pointer/three_sum/optimal.js
@@ -7,38 +7,39 @@ function threeSum(array, target) {
 
     for (i = 0; i < n - 2; i++) {
 
-
         // skip duplicate values for i
-        while (i > 0 && array[i] === array[i - 1]) {
-            let left = i + 1
-            let right = n - 1
-            while (left < right) {
-                let sum = array[i] + array[left] + array[right]
+        if (i > 0 && array[i] === array[i - 1]) {
+            continue
+        }
+        let left = i + 1
+        let right = n - 1
+        while (left < right) {
+            let sum = array[i] + array[left] + array[right]
 
-                if (sum === 0) {
-                    result.push([array[i], array[left], array[right]])
+            if (sum === target) {
+                result.push([array[i], array[left], array[right]])
 
-                    left++;
-                    right--
-                    while (array[left] === array[left - 1]) {
-                        left++
-                    }
-
-                    while (array[right] === array[right + 1]) {
-                        right--
-                    }
-                } else if (sum < 0) {
+                left++;
+                right--
+                while (array[left] === array[left - 1]) {
                     left++
-                } else {
-                    right--
                 }
 
+                while (array[right] === array[right + 1]) {
+                    right--
+                }
+            } else if (sum < target) {
+                left++
+            } else {
+                right--
             }
-
 
         }
 
+
     }
-    return result 
+    return result
 }
-console.log(threeSum([-1,0,1,2,-1,-4],0))
+
+
+console.log(threeSum([-1, 0, 1, 2, -1, -4], 0))
