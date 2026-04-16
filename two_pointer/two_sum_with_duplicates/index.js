9ccef9f57cf59c459ed76a69662fa30fac12d4b4
Shilpa-pal <shilpapal1721@gmail.com>
2026-04-16 11:18:56 +0530

    Update index.js

--- two_pointer/two_sum_with_duplicates/index.js
+++ two_pointer/two_sum_with_duplicates/index.js
@@ -2,22 +2,23 @@
 // give some of dulplicates
 function sumOfDuplicates(array,target){
     let n = array.length
-    let left = 0
-    let right = n-1
+    let left = 0 // starts at beginning
+    let right = n-1  // starts at end
     let result = []
     while(left <right){
         let sum = array[left] + array[right]
 
         if(sum === target){
             result.push(array[left],array[right])
-            while(array[left] === array[left -1]){
+            // skip while next is same
+            while(array[left] === array[left +1]){// skip left duplicates
                 left ++
             }
-            while(array[right] === array[right +1]){
+            while(array[right] === array[right -1]){//// skip right duplicates
                 right--
             }
-            left++
-            right--
+            left++ // move past the last duplicate
+            right--// move past the last duplicate
         }
         else if(sum <target){
             left ++
